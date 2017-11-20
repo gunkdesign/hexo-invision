ff2af40c574bcb99371b8a204c5573eeda7409b9
Andys Macbook Pro <andy@gunkdesign.co.uk>
2017-11-20 22:29:37 +0100

    Fixed issue on width constant

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // {% invision projectId height width %}
 function invisionTagRender(args) {
   const id = args[0];
-  const height = args[1] != null ? args[1] : 424;
-  const width = args[2] != null ? args[5] : 916;
+  const height = args[1] != null ? args[1] : 916;
+  const width = args[2] != null ? args[2] : 424;
 
   return `<iframe width="${width}" height="${height}" src="//invis.io/${id}" frameborder="0" allowfullscreen></iframe>`;
 }
